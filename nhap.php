7f4e98279b9c7b20e6bce2c79b1d6b75eb7222b9
ChiHai1801 <101734887+ChiHai1801@users.noreply.github.com>
2022-03-20 15:15:17 +0700

    Add files via upload

---
+++ nhap.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Nhập mật khẩu <input type="password" name="pwd"><br>
+ngày sinh <input type="date" name="nsinh"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Hello <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your date of birth: <?php echo $_POST["nsinh"]; ?>
+
+</body>
+</html>
